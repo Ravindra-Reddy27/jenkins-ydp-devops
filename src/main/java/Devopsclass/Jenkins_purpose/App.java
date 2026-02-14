58fcc384d4cc533e0d8f67413915944046e8f7e3
reddyravindr2005@gmail.com <reddyravindra2005@gmail.com>
2026-02-14 14:39:52 +0530

    Initial commit

---
+++ src/main/java/Devopsclass/Jenkins_purpose/App.java
@@ -0,0 +1,10 @@
+package Devopsclass.Jenkins_purpose;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
